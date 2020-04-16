5c5a87855d495dbc8010619b4d9b4bc0cef52043
Fernando <fernando.bragga@gmail.com>
2020-04-16 22:28:47 +1000

    discover page

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+
+  private _places: Place[] = [
+    new Place(
+      'p1', 
+      'Manhattan Mansion', 
+      'In the heart of New York City',
+      'https://lp-cms-production.imgix.net/2019-06/GettyImages-538096543_medium.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      149.99
+    ),
+    new Place(
+      'p2',
+      'L\'Amour Toujours',
+      'A romantic place in Paris!',
+      'https://photos.mandarinoriental.com/is/image/MandarinOriental/paris-2017-home?$MO_masthead-property-mobile$',
+      189.99
+    ),
+    new Place(
+      'p3',
+      'The Foggy Palace',
+      'Not your average city trip',
+      'https://data.whicdn.com/images/310015645/original.jpg',
+      99.99
+    )
+  ];
+
+  get places(){
+    return [...this._places];
+  }
+  
+  
+  constructor() { }
+
+
+
+}
